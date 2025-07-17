57b33c844d855b40d79fdfd8914c1c4bef566cdb
Manjunath063 <manjunathjangam198@gmail.com>
2025-07-17 16:04:10 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -30,10 +30,10 @@ console.log('Database Configuration:', {
 
 // CORS configuration
 const allowedOrigins = [
-  'http://54.166.206.245:8005',
-  'http://54.166.206.245:8006',
-  'http://54.166.206.245:8007',
-  process.env.FRONTEND_URL || 'http://54.166.206.245:8005'
+  'http://16.171.145.31:8005',
+  'http://16.171.145.31:8006',
+  'http://16.171.145.31:8007',
+  process.env.FRONTEND_URL || 'http://16.171.145.31:8005'
 ];
 
 app.use(cors({
